c39e378fa151b0f24ffdf42fe93c71ea21d96433
harshini <harshinipamarthi@gmail.com>
2025-03-06 20:12:39 +0530

    n

--- src/main/java/com/example/UiApplication.java
+++ src/main/java/com/example/UiApplication.java
@@ -2,10 +2,13 @@ package com.example.uipage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class UiApplication {
     public static void main(String[] args) {
-        SpringApplication.run(UiApplication.class, args);
+        new SpringApplicationBuilder(UiApplication.class)
+                .properties("server.port=8084")
+                .run(args);
     }
 }
