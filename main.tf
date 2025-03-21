bd557b60d2437a707eadf12a83037f7dc04ae9ae
harshini <harshinipamarthi@gmail.com>
2025-03-21 18:19:26 +0530

    infra

---
+++ main.tf
@@ -0,0 +1,22 @@
+resource "google_container_cluster" "primary" {
+    name = var.GKE_CLUSTER 
+    location = var.GKE_REGION
+    deletion_protection = false
+    remove_default_node_pool = true 
+    initial_node_count = var.NODE_COUNT
+    node_config {
+        machine_type = var.MACHINE_TYPE 
+        disk_size_gb = 30
+        oauth_scopes = [
+            "https://www.googleapis.com/auth/cloud-platform"
+        ]
+    }
+    
+}
+output "cluster_name" {
+  value = google_container_cluster.primary.name
+}
+output "kubeconfig" {
+    value = google_container_cluster.primary.endpoint
+}
+
---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "PROJECT_ID" {
+    description = "gcp project ID"
+    type = string
+    default = "harshini-project-452710"
+}
+
+variable "GKE_REGION"{
+    description = "default region where resources will be created"
+    type = string
+    default = "us-central1"
+}
+
+variable "GKE_CLUSTER"{
+    description = "name of GKE cluster"
+    type = string
+    default = "usecase-1-cluster"
+}
+
+variable "MACHINE_TYPE" {
+    description = "machine type for nodes of cluster"
+    type = string
+    default = "e2-medium"
+}
+
+variable "NODE_COUNT"{
+    description = "number of nodes"
+    type = number
+    default = 1
+}
