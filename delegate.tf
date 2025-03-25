808a006171f097d1e4c78945e78d58c1a0277a8e
harshini <harshinipamarthi@gmail.com>
2025-03-25 15:28:17 +0530

    Update delegate.tf

--- delegate.tf
+++ delegate.tf
@@ -23,7 +23,6 @@ provider "helm" {
     host                   = "https://${data.google_container_cluster.gke_cluster.endpoint}"
     token                  = data.google_client_config.default.access_token
     cluster_ca_certificate = base64decode(data.google_container_cluster.gke_cluster.master_auth[0].cluster_ca_certificate)
-    config_path = "/home/harshini_lakshmipriya/.kube/config"
   }
 }
 
