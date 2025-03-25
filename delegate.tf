9daced976678dc49ac335c64e33c5b27af8fa641
harshini <harshinipamarthi@gmail.com>
2025-03-25 15:16:38 +0530

    Update delegate.tf

--- delegate.tf
+++ delegate.tf
@@ -23,8 +23,10 @@ provider "helm" {
     host                   = "https://${data.google_container_cluster.gke_cluster.endpoint}"
     token                  = data.google_client_config.default.access_token
     cluster_ca_certificate = base64decode(data.google_container_cluster.gke_cluster.master_auth[0].cluster_ca_certificate)
+    config_path = "~/.kube/config"
   }
 }
+
 
 module "delegate" {
   source = "harness/harness-delegate/kubernetes"
