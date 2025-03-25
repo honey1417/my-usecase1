8a46a192cffea9e85c0984e2281b26bc3b8c1232
harshini <harshinipamarthi@gmail.com>
2025-03-25 18:39:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,28 @@
 resource "google_container_cluster" "primary" {
-    name = var.cluster_name
-    location = var.region
-    deletion_protection = false
-    remove_default_node_pool = true
-    initial_node_count = var.NODE_COUNT
-    node_config {
-        machine_type = var.MACHINE_TYPE 
-        disk_size_gb = 70
-        oauth_scopes = [
-            "https://www.googleapis.com/auth/cloud-platform"
-        ]
-    }
-    
+  name               = var.cluster_name
+  location           = var.region
+  deletion_protection = false
+  remove_default_node_pool = true  # initial_node_count is ignored when the default node pool is removed.
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "primary-node-pool"
+  cluster    = google_container_cluster.primary.name
+  location   = var.region
+  initial_node_count = var.NODE_COUNT
+  node_config {
+    machine_type = var.MACHINE_TYPE
+    disk_size_gb = 70
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
 }
 
 output "cluster_name" {
   value = google_container_cluster.primary.name
 }
-output "kubeconfig" {
-    value = google_container_cluster.primary.endpoint
-}
 
+output "kubeconfig" {
+  value = google_container_cluster.primary.endpoint
+}
